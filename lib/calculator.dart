afcf872680e6ab060c0abcf7f585604ff68a794a
Sumeyye Korkmaz <20190309032@dogus.edu.tr>
2022-08-30 19:56:43 +0300

    finish commit

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -17,10 +17,13 @@ class _CalculatorState extends State<Calculator> {
 
   Widget buttondizayn(String butondegeri) {
     return Container(
-      margin: EdgeInsets.all(6),
+      margin: EdgeInsets.all(5),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           primary: Colors.white,
+          side: BorderSide(width: 1.5, color: Colors.indigoAccent),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         ),
         onPressed: () {
           setState(() {
@@ -37,8 +40,9 @@ class _CalculatorState extends State<Calculator> {
               num2 = int.parse(sonuc);
 
               toplam = (num1 + num2).toString();
-
-
+              eski = toplam;
+            } else if (butondegeri == '<') {
+              toplam = sonuc.substring(0, sonuc.length - 1);
             } else {
               toplam = toplam + butondegeri;
             }
@@ -46,7 +50,13 @@ class _CalculatorState extends State<Calculator> {
             sonuc = int.parse(toplam).toStringAsFixed(0);
 
             if (butondegeri != 'C') {
-              eski = eski + butondegeri;
+              if (butondegeri == '=') {
+                eski = eski;
+              } else if (butondegeri == '<') {
+                eski = eski.substring(0, eski.length - 1);
+              } else {
+                eski = eski + butondegeri;
+              }
             } else {
               eski = '';
             }
@@ -68,90 +78,117 @@ class _CalculatorState extends State<Calculator> {
     return Scaffold(
       backgroundColor: Color(0xFFbdc6cf),
       appBar: AppBar(
-        title: Text(
-          "Calculator",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 30,
-          ),
+        toolbarHeight: 60,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Calculator ",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+              ),
+            ),
+            Icon(
+              Icons.calculate,
+              size: 35,
+            )
+          ],
         ),
         centerTitle: true,
         backgroundColor: Colors.indigoAccent,
       ),
       body: Container(
-          child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 80),
-            child: Padding(
-              padding: const EdgeInsets.only(right: 50),
-              child: Container(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  eski,
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.black,
+        child: Column(
+          children: [
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 80),
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 60),
+                    child: Container(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        eski,
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 15, right: 50, bottom: 15),
-            child: Container(
-              alignment: Alignment.centerRight,
-              child: Text(
-                sonuc,
-                style: TextStyle(
-                  fontSize: 85,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 50),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    buttondizayn("7"),
-                    buttondizayn("8"),
-                    buttondizayn("9"),
-                  ],
-                ),
-                Row(
-                  children: [
-                    buttondizayn("4"),
-                    buttondizayn("5"),
-                    buttondizayn("6"),
-                  ],
-                ),
-                Row(
-                  children: [
-                    buttondizayn("1"),
-                    buttondizayn("2"),
-                    buttondizayn("3"),
-                  ],
-                ),
-                Row(
-                  children: [
-                    buttondizayn("C"),
-                    buttondizayn("0"),
-                    buttondizayn("+"),
-                  ],
-                ),
-                Row(
-                  children: [
-                    buttondizayn("="),
-                  ],
+                Padding(
+                  padding:
+                      const EdgeInsets.only(top: 15, right: 50, bottom: 15),
+                  child: Container(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      sonuc,
+                      style: TextStyle(
+                        fontSize: 85,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
-      )),
+            Divider(thickness: 2, color: Colors.indigoAccent,height: 40,indent: 30,endIndent: 30,),
+            Padding(
+              padding: const EdgeInsets.only(left: 30),
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      buttondizayn("7"),
+                      buttondizayn("4"),
+                      buttondizayn("1"),
+                      buttondizayn("C"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      buttondizayn("8"),
+                      buttondizayn("5"),
+                      buttondizayn("2"),
+                      buttondizayn("0"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      buttondizayn("9"),
+                      buttondizayn("6"),
+                      buttondizayn("3"),
+                      buttondizayn("<"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        child: buttondizayn("+"),
+                        height: 300,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30),
+              child: Row(
+                children: [
+                  Container(
+                    child: buttondizayn("="),
+                    width: 275,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
